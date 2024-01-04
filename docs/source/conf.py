c237e49598b1bafb75b5422013da5e074691fdb4
Matthias Toberer <matthias.toberer@uni-ulm.de>
2024-01-04 16:05:43 +0100

    change project informations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MECFS Kompass'
+copyright = '2024, MaTo'
+author = 'MaTo'
 
 release = '0.1'
 version = '0.1.0'
